97f500b2fa1ce23e3bdb22dcc47132a8728f9b98
Yoon-juhan <yoonjuhan00@gmail.com>
2023-10-25 22:45:07 +0900

    카테고리당 5페이지 크롤링

--- test.py
+++ test.py
@@ -1,29 +1,28 @@
 from bs4 import BeautifulSoup            
 from selenium import webdriver           
 import time                              
-from selenium.webdriver.common.by import By
 
-# 브라우저 켜기
 browser = webdriver.Chrome()
 
 url_list = []
-for i in range(1, 3):
+for category in range(6):
+    for page in range(1, 6):
+        url = f'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1={100 + category}#&date=%2000:00:00&page={page}'
+        browser.get(url)
 
-    url = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=105#&date=%2000:00:00&page=' + str(i)
-    browser.get(url)
+        time.sleep(1)
 
-    time.sleep(1)
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+        a_list = soup.select(".type06_headline dt+dt a")
+        a_list.extend(soup.select(".type06 dt+dt a"))
 
-    soup = BeautifulSoup(browser.page_source, "html.parser")
+        
+        for a in a_list:
+            if "href" in a.attrs:
+                if ("article" in a["href"]) and (f"sid={100+category}" in a["href"]):
+                    url_list.append((a["href"], a.text))
 
-    a_list = soup.select(".type06_headline dt+dt a")
-    a_list.extend(soup.select(".type06 dt+dt a"))
-
-    
-    for a in a_list:
-        if "href" in a.attrs:
-            if ("article" in a["href"]) and ("sid=105" in a["href"]):
-                url_list.append((a["href"], a.text))
+        print(category, page)
 
 print(url_list)
 print(len(url_list))
